0398a6c15b0d53b25066b24a195efafffabbbf17
Carlos Gonzalez Betancort <carlos.gonzalez@couchbase.com>
2021-04-12 14:25:20 +0100

    MB-45153 Add grpcutils package
    The package has code to extract token auth from gRPC calls.
    
    Change-Id: Iaa460f9a10b96054cfc17cbdfb8a2c03a4615bcd
    Reviewed-on: http://review.couchbase.org/c/tools-common/+/150960
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: James Lee <james.lee@couchbase.com>

--- go.mod
+++ go.mod
@@ -8,5 +8,6 @@ require (
 	github.com/mattn/go-sqlite3 v1.14.6
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/sys v0.0.0-20210225080010-8e9945a5478f
+	google.golang.org/grpc v1.37.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
---
+++ grpcutil/token.go
@@ -0,0 +1,33 @@
+package grpcutil
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"google.golang.org/grpc/metadata"
+)
+
+// ExtractToken gets an auth token from the context of a gRPC call.
+func ExtractToken(ctx context.Context, scheme string) (string, error) {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return "", fmt.Errorf("no metadata found")
+	}
+
+	auths := md.Get("authorization")
+	if len(auths) == 0 {
+		return "", fmt.Errorf("no authorization headers")
+	}
+
+	authTokenParts := strings.SplitN(auths[0], " ", 2)
+	if len(authTokenParts) < 2 {
+		return "", fmt.Errorf("invalid auth token")
+	}
+
+	if !strings.EqualFold(scheme, authTokenParts[0]) {
+		return "", fmt.Errorf("invalid auth scheme")
+	}
+
+	return authTokenParts[1], nil
+}
