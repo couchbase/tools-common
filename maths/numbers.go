fa5ab506bc401110ab15191121d130e8d5b56086
James Lee <james.lee@couchbase.com>
2021-05-18 18:04:00 +0100

    MB-46368 Add min/max functions for int64/uint32
    Change-Id: I1f8a344932312a3d6139f06098f6aaed6c150cab
    Reviewed-on: http://review.couchbase.org/c/tools-common/+/153836
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Carlos Gonzalez <carlos.gonzalez@couchbase.com>

--- maths/numbers.go
+++ maths/numbers.go
@@ -18,6 +18,42 @@ func MaxUint64(a, b uint64) uint64 {
 	return b
 }
 
+// MinInt64 returns the smallest of the two int64s given as input.
+func MinInt64(a, b int64) int64 {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
+// MaxInt64 returns the largest of the two int64s given as input.
+func MaxInt64(a, b int64) int64 {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+// MinUint32 returns the smallest of the two uint32s given as input.
+func MinUint32(a, b uint32) uint32 {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
+// MaxUint32 returns the largest of the two uint32s given as input.
+func MaxUint32(a, b uint32) uint32 {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
 // Min returns the smallest of the two ints given as input.
 func Min(a, b int) int {
 	if a < b {
