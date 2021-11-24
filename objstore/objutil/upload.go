959aae935d816abae11bb75e7e08fff25e452b9b
James Lee <james.lee@couchbase.com>
2021-11-24 21:20:42 +0000

    MB-49759 Add a generic 'MPUploader'
    Change-Id: I5dec450d98888a2828dfc8ee8a151f965e24b611
    Reviewed-on: https://review.couchbase.org/c/tools-common/+/166378
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Patrick Varley <patrick@couchbase.com>

---
+++ objstore/objutil/upload.go
@@ -0,0 +1,111 @@
+// Package objutil provide utility functions for object store clients which expose more complex/configurable behavior
+// than using a base 'objcli.Client'.
+package objutil
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/couchbase/tools-common/maths"
+	"github.com/couchbase/tools-common/objstore/objcli"
+
+	"github.com/aws/aws-sdk-go/aws"
+)
+
+const (
+	// MinPartSize is the minimum size allowed for 'PartSize', and is a hard limit enforced by AWS.
+	MinPartSize = 5*1024*1024 + 1
+
+	// MPUThreshold is the threshold at which point we break the upload up into multiple requests which are executed
+	// concurrently.
+	//
+	// NOTE: Multipart uploads generally require at least three requests, hence the choice of 'MinPartSize * 3'.
+	MPUThreshold = MinPartSize * 3
+)
+
+// ReadAtSeeker is a composition of the reader/seeker/reader at interfaces.
+type ReadAtSeeker interface {
+	io.Reader
+	io.Seeker
+	io.ReaderAt
+}
+
+// UploadOptions encapsulates the options available when using the 'Upload' function to upload data to a remote cloud.
+type UploadOptions struct {
+	// Client is the client used to perform the operation.
+	//
+	// NOTE: This attribute is required.
+	Client objcli.Client
+
+	// Bucket is the bucket to upload the object to.
+	//
+	// NOTE: This attribute is required.
+	Bucket string
+
+	// Key is the key for the object being uploaded.
+	//
+	// NOTE: This attribute is required.
+	Key string
+
+	// Body is the content which should be used for the body of the object.
+	//
+	// NOTE: This attribute is required.
+	Body ReadAtSeeker
+
+	// PartSize indicates what size should be used for individual parts when being uploaded using a multipart upload.
+	PartSize int64
+
+	// MPUThreshold is a threshold at which point objects which broken down into multipart uploads.
+	MPUThreshold int64
+}
+
+// defaults populates the options with sensible defaults.
+func (u *UploadOptions) defaults() {
+	u.PartSize = maths.MaxInt64(u.PartSize, MinPartSize)
+	u.MPUThreshold = maths.MaxInt64(u.MPUThreshold, MPUThreshold)
+}
+
+// Upload an object to a remote cloud breaking it down into a multipart upload if the body is over a given size.
+func Upload(opts UploadOptions) error {
+	// Fill out any missing fields with the sane defaults
+	opts.defaults()
+
+	length, err := aws.SeekerLen(opts.Body)
+	if err != nil {
+		return fmt.Errorf("failed to determine length of body: %w", err)
+	}
+
+	// Under the threshold, upload using a single request
+	if length <= opts.MPUThreshold {
+		return opts.Client.PutObject(opts.Bucket, opts.Key, opts.Body)
+	}
+
+	return upload(opts)
+}
+
+// upload an object to a remote cloud by breaking it down into individual chunks and uploading them concurrently.
+func upload(opts UploadOptions) error {
+	mpu, err := NewMPUploader(MPUploaderOptions{
+		Client: opts.Client,
+		Bucket: opts.Bucket,
+		Key:    opts.Key,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create uploader: %w", err)
+	}
+	defer mpu.Abort() //nolint:errcheck,wsl
+
+	reader := NewChunkReader(opts.Body, opts.PartSize)
+
+	err = reader.ForEach(func(chunk *io.SectionReader) error { return mpu.Upload(chunk) })
+	if err != nil {
+		return fmt.Errorf("failed to queue chunks: %w", err)
+	}
+
+	err = mpu.Commit()
+	if err != nil {
+		return fmt.Errorf("failed to complete upload: %w", err)
+	}
+
+	return nil
+}
---
+++ objstore/objutil/upload_test.go
@@ -0,0 +1,52 @@
+package objutil
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"github.com/couchbase/tools-common/objstore/objcli"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestUploadOptionsDefaults(t *testing.T) {
+	options := UploadOptions{}
+	options.defaults()
+	require.Equal(t, int64(MinPartSize), options.PartSize)
+	require.Equal(t, int64(MPUThreshold), options.MPUThreshold)
+}
+
+func TestUploadObjectLessThanThreshold(t *testing.T) {
+	client := objcli.NewTestClient(t)
+
+	options := UploadOptions{
+		Client: client,
+		Bucket: "bucket",
+		Key:    "key",
+		Body:   strings.NewReader("body"),
+	}
+
+	require.NoError(t, Upload(options))
+	require.Len(t, client.Buckets, 1)
+	require.Len(t, client.Buckets["bucket"], 1)
+	require.Contains(t, client.Buckets["bucket"], "key")
+	require.Equal(t, []byte("body"), client.Buckets["bucket"]["key"].Body)
+}
+
+func TestUploadObjectGreaterThanThreshold(t *testing.T) {
+	client := objcli.NewTestClient(t)
+
+	options := UploadOptions{
+		Client: client,
+		Bucket: "bucket",
+		Key:    "key",
+		Body:   bytes.NewReader(make([]byte, MPUThreshold+1)),
+	}
+
+	require.NoError(t, Upload(options))
+	require.Len(t, client.Buckets, 1)
+	require.Len(t, client.Buckets["bucket"], 1)
+	require.Contains(t, client.Buckets["bucket"], "key")
+	require.Equal(t, make([]byte, MPUThreshold+1), client.Buckets["bucket"]["key"].Body)
+}
