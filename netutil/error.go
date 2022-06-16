90cae8b2c7e622991545f65a95e7460dc59359ee
Maksimiljans Januska <maks.januska@couchbase.com>
2022-06-16 11:31:36 +0100

    MB-52401 Increase ResponseHeaderTimeout and retry if reached it
    Prior to this change REST requests to the CBAS could fail due to
    a fairly low default ResponseHeaderTimeout value of our REST HTTP
    client. Now this tiemout has been increased to 30 seconds from 10
    to alleviate the issue.
    
    Additionally, previously we would not retry idempotent requests
    that had failed due to this timeout but would, for example, retry
    on failures due to the TLS handshake timeout. Now we retry a
    request if we get a standard Go library error that corresponds to
    this timeout.
    
    Change-Id: I830c9950ac7b08a8cf50de95fed477dcbf51a038
    Reviewed-on: https://review.couchbase.org/c/tools-common/+/176286
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: James Lee <james.lee@couchbase.com>

--- netutil/error.go
+++ netutil/error.go
@@ -10,19 +10,20 @@ import (
 // TemporaryErrorMessages is a slice of known error messages which may be returned by the Go standard library when
 // attempting to perform network operations.
 var TemporaryErrorMessages = []string{
-	"bad record MAC",                   // src/crypto/tls/alert.go
-	"broken pipe",                      // src/syscall/zerrors_linux_amd64.go
-	"connection refused",               // src/syscall/zerrors_linux_amd64.go
-	"connection reset",                 // src/syscall/zerrors_linux_amd64.go
-	"connection timed out",             // src/syscall/zerrors_linux_amd64.go
-	"http: ContentLength=",             // src/net/http/transfer.go
-	"i/o timeout",                      // src/net/net.go
-	"net/http: TLS handshake timeout",  // src/net/http/transport.go
-	"server closed idle connection",    // src/net/http/transport.go
-	"stream error:",                    // src/net/http/h2_bundle.go
-	"transport connection broken",      // src/net/http/transport.go
-	"unexpected EOF reading trailer",   // src/net/http/transfer.go
-	"use of closed network connection", // src/internal/poll/fd.go
+	"bad record MAC",                              // src/crypto/tls/alert.go
+	"broken pipe",                                 // src/syscall/zerrors_linux_amd64.go
+	"connection refused",                          // src/syscall/zerrors_linux_amd64.go
+	"connection reset",                            // src/syscall/zerrors_linux_amd64.go
+	"connection timed out",                        // src/syscall/zerrors_linux_amd64.go
+	"http: ContentLength=",                        // src/net/http/transfer.go
+	"i/o timeout",                                 // src/net/net.go
+	"net/http: TLS handshake timeout",             // src/net/http/transport.go
+	"net/http: timeout awaiting response headers", // src/net/http/transport.go
+	"server closed idle connection",               // src/net/http/transport.go
+	"stream error:",                               // src/net/http/h2_bundle.go
+	"transport connection broken",                 // src/net/http/transport.go
+	"unexpected EOF reading trailer",              // src/net/http/transfer.go
+	"use of closed network connection",            // src/internal/poll/fd.go
 }
 
 // IsTemporaryError returns a boolean indicating whether the provided error is a result of a temporary failure and
