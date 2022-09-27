93a8eda4bd6509fc2903053334c3efd71b651b27
Matt Hall <matt.hall@couchbase.com>
2022-09-27 09:40:29 +0100

    MB-53826 Add objutil.CompressObjects funtion
    This function can be used to zip up a number of objects (specified in
    the same way as Client.IterateObjects) and upload that zip to cloud
    storage on the fly.
    
    It is currently largely sequential - a file is downloaded, it's zipped
    and periodically the zip is flushed to cloud storage.
    
    Change-Id: I67bfa1f1db47de3e3748c4f12ccffc31bdf3a110
    Reviewed-on: https://review.couchbase.org/c/tools-common/+/180453
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: James Lee <james.lee@couchbase.com>

---
+++ objstore/objutil/compress_upload.go
@@ -0,0 +1,244 @@
+package objutil
+
+import (
+	"archive/zip"
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"regexp"
+
+	"github.com/couchbase/tools-common/freelist"
+	"github.com/couchbase/tools-common/hofp"
+	"github.com/couchbase/tools-common/objstore/objcli"
+	"github.com/couchbase/tools-common/objstore/objcli/objaws"
+	"github.com/couchbase/tools-common/objstore/objval"
+)
+
+// PartCompleteFunc is called once a part of the zip file has been uploaded. size is the size of the part uploaded.
+type PartCompleteFunc func(size int)
+
+// CompressObjectsOpts specifies options which configure what and how objects are compressed and uploaded.
+type CompressObjectsOpts struct {
+	Context context.Context
+
+	// Client is the objcli.Client to use to download and upload.
+	//
+	// NOTE: required
+	Client objcli.Client
+
+	// PartSize is the size of parts to upload.
+	PartSize int64
+
+	// PartUploadWorkers is the number of parts to upload at once.
+	PartUploadWorkers int
+
+	// PartCompleteCallback is called once a part has been uploaded.
+	PartCompleteCallback PartCompleteFunc
+
+	// SourceBucket is the bucket to compress objects from.
+	//
+	// NOTE: required
+	SourceBucket string
+
+	// Prefix is the prefix of objects to compress.
+	//
+	// NOTE: required
+	Prefix string
+
+	// Delimiter is used when iterating through the objects that begin with Prefix.
+	Delimiter string
+
+	// Include allows you to include certain keys in the zip by a regular expression.
+	Include []*regexp.Regexp
+
+	// Exclude allows you to exclude certain keys from being in the zip by a regular expression.
+	Exclude []*regexp.Regexp
+
+	// DestinationBucket is the bucket to upload to.
+	//
+	// NOTE: required
+	DestinationBucket string
+
+	// Destination is the key to give the zip that is uploaded.
+	//
+	// NOTE: required
+	Destination string
+}
+
+func (o *CompressObjectsOpts) defaults() {
+	if o.Context == nil {
+		o.Context = context.Background()
+	}
+
+	if o.PartSize == 0 {
+		o.PartSize = objaws.MinUploadSize
+	}
+
+	if o.PartUploadWorkers == 0 {
+		// For each worker we create a buffer of PartSize bytes. This means we probably want to keep PartUploadWorkers
+		// fairly small, particularly as it is unlikely we will upload many parts at a time given download is likely to be
+		// the bottleneck
+		//
+		// NOTE: We may reconsider this. See https://issues.couchbase.com/browse/MB-53854
+		o.PartUploadWorkers = 4
+	}
+}
+
+// download streams the given object and writes it to zipWriter.
+func download(ctx context.Context, cli objcli.Client, bucket, key string, zipWriter *zip.Writer) error {
+	writer, err := zipWriter.Create(key)
+	if err != nil {
+		return fmt.Errorf("could not create file in zip: %w", err)
+	}
+
+	obj, err := cli.GetObject(ctx, bucket, key, nil)
+	if err != nil {
+		return fmt.Errorf("could not get object '%s': %w", key, err)
+	}
+	defer obj.Body.Close()
+
+	_, err = io.Copy(writer, obj.Body)
+	if err != nil {
+		return fmt.Errorf("could not copy object into zip: %w", err)
+	}
+
+	return nil
+}
+
+// iterate calls download on each object that matches the iterate parameters given in opts.
+func iterate(ctx context.Context, opts CompressObjectsOpts, zipWriter *zip.Writer) error {
+	fn := func(attrs *objval.ObjectAttrs) error {
+		if attrs.IsDir() {
+			return nil
+		}
+
+		return download(ctx, opts.Client, opts.SourceBucket, attrs.Key, zipWriter)
+	}
+
+	err := opts.Client.IterateObjects(ctx,
+		opts.SourceBucket,
+		opts.Prefix,
+		opts.Delimiter,
+		opts.Include,
+		opts.Exclude,
+		fn,
+	)
+	if err != nil {
+		return fmt.Errorf("error whilst iterating objects: %w", err)
+	}
+
+	return nil
+}
+
+// uploadFromReader reads from reader and sends it using opts.Client to opts.Destination.
+//
+// NOTE: It does this by keeping opts.PartUploadWorkers internal buffers, reading into those and uploading them as parts
+// of the final object. Having multiple buffers means we do not need to wait for a part to be uploaded to start reading
+// from reader again.
+func uploadFromReader(ctx context.Context, opts CompressObjectsOpts, reader io.Reader) error {
+	fl := freelist.NewFreeListWithFactory(opts.PartUploadWorkers, func() []byte { return make([]byte, opts.PartSize) })
+
+	// payload is the metadata we pass when uploading so that we can give the buffer back to the freelist and call
+	// opts.PartCompleteCallback with the correct size.
+	type payload struct {
+		buf  []byte
+		size int
+	}
+
+	// onComplete returns the slice to the freelist and calls the user's callback.
+	onComplete := func(metadata any, part objval.Part) error {
+		payload, _ := metadata.(*payload)
+		if err := fl.Put(ctx, payload.buf); err != nil {
+			return fmt.Errorf("could not return buffer to freelist: %w", err)
+		}
+
+		if opts.PartCompleteCallback != nil {
+			opts.PartCompleteCallback(payload.size)
+		}
+
+		return nil
+	}
+
+	mp, err := NewMPUploader(MPUploaderOptions{
+		Client:         opts.Client,
+		Bucket:         opts.DestinationBucket,
+		Key:            opts.Destination,
+		OnPartComplete: onComplete,
+		Options:        Options{PartSize: opts.PartSize, Context: ctx},
+	})
+	if err != nil {
+		return fmt.Errorf("could not create uploader: %w", err)
+	}
+	defer mp.Abort() //nolint:errcheck,wsl
+
+	// Repeatedly fill up parts (slices from the freelist) and upload them until we reach the end of reader
+	for {
+		slice, err := fl.Get(ctx)
+		if err != nil {
+			return fmt.Errorf("could not get buffer from freelist: %w", err)
+		}
+
+		pos, readErr := io.ReadFull(reader, slice)
+		if readErr != nil && !errors.Is(readErr, io.EOF) && !errors.Is(readErr, io.ErrUnexpectedEOF) {
+			return fmt.Errorf("could not read: %w", readErr)
+		}
+
+		buf := bytes.NewReader(slice[:pos])
+
+		if err = mp.UploadWithMeta(&payload{buf: slice, size: pos}, buf); err != nil {
+			return fmt.Errorf("could not upload part: %w", err)
+		}
+
+		// If readErr != nil and we didn't return early it means we hit an EOF
+		if readErr != nil {
+			break
+		}
+	}
+
+	if err = mp.Commit(); err != nil {
+		return fmt.Errorf("could not commit upload: %w", err)
+	}
+
+	return nil
+}
+
+// CompressObjects takes an object storage prefix and a destination. It will create a zip in destination and compress
+// and upload every object with the given prefix there. Each object will be streamed from cloud storage, through a
+// ZipWriter and back to cloud storage.
+func CompressObjects(opts CompressObjectsOpts) error {
+	if opts.Client == nil || opts.SourceBucket == "" || opts.Prefix == "" || opts.DestinationBucket == "" ||
+		opts.Destination == "" {
+		return fmt.Errorf("missing required parameters")
+	}
+
+	opts.defaults()
+
+	var (
+		r, w      = io.Pipe()
+		zipWriter = zip.NewWriter(w)
+
+		pool = hofp.NewPool(hofp.Options{Context: opts.Context, Size: 2})
+	)
+
+	pool.Queue(func(ctx context.Context) error { //nolint:errcheck
+		if err := iterate(ctx, opts, zipWriter); err != nil {
+			return fmt.Errorf("could not iterate through objects: %w", err)
+		}
+
+		if err := zipWriter.Close(); err != nil {
+			return fmt.Errorf("could not close zip writer: %w", err)
+		}
+
+		if err := w.Close(); err != nil {
+			return fmt.Errorf("could not close pipe writer: %w", err)
+		}
+
+		return nil
+	})
+
+	pool.Queue(func(ctx context.Context) error { return uploadFromReader(ctx, opts, r) }) //nolint:errcheck
+
+	return pool.Stop()
+}
---
+++ objstore/objutil/compress_upload_test.go
@@ -0,0 +1,112 @@
+package objutil
+
+import (
+	"archive/zip"
+	"bytes"
+	"context"
+	"io"
+	"math/rand"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/couchbase/tools-common/objstore/objcli"
+	"github.com/couchbase/tools-common/objstore/objcli/objaws"
+	"github.com/couchbase/tools-common/objstore/objval"
+)
+
+func TestCompressUploadOptsDefaults(t *testing.T) {
+	opts := CompressObjectsOpts{
+		Client:            objcli.NewTestClient(t, objval.ProviderAWS),
+		SourceBucket:      "bucket",
+		DestinationBucket: "bucket",
+		Prefix:            "prefix",
+		Destination:       "dest",
+	}
+
+	opts.defaults()
+
+	require.Equal(t, CompressObjectsOpts{
+		SourceBucket:      "bucket",
+		DestinationBucket: "bucket",
+		Prefix:            "prefix",
+		Destination:       "dest",
+		PartSize:          objaws.MinUploadSize,
+		PartUploadWorkers: 4,
+
+		// These fields we're not interesting in checking, so ensure they're always equal to the actual result
+		Context: opts.Context,
+		Client:  opts.Client,
+	}, opts)
+}
+
+func TestCompressUploadOptsMissing(t *testing.T) {
+	require.Error(t, CompressObjects(CompressObjectsOpts{}))
+}
+
+func TestCompressUpload(t *testing.T) {
+	const PartSize = 1024
+
+	var (
+		testData = make([]byte, 1024)
+		n, err   = rand.Read(testData)
+	)
+
+	require.NoError(t, err)
+	require.Equal(t, len(testData), n)
+
+	cli := objcli.NewTestClient(t, objval.ProviderAWS)
+
+	// Test a few different cases: objects the size of PartSize, an odd size (i.e. not divisable or a factor of PartSize)
+	// and a multiple of PartSize
+	paths := []struct {
+		path string
+		size int64
+	}{
+		{path: "prefix/foo.txt", size: PartSize},
+		{path: "prefix/1/bar.txt", size: 147},
+		{path: "prefix/1/2/baz.txt", size: 4 * PartSize},
+	}
+
+	for _, path := range paths {
+		var (
+			buf    = make([]byte, path.size)
+			n, err = rand.Read(buf)
+		)
+
+		require.NoError(t, err)
+		require.Equal(t, len(buf), n)
+		require.NoError(t, cli.PutObject(context.Background(), "bucket", path.path, bytes.NewReader(buf)))
+	}
+
+	require.NoError(t, CompressObjects(CompressObjectsOpts{
+		Client:            cli,
+		PartSize:          PartSize,
+		SourceBucket:      "bucket",
+		Prefix:            "prefix",
+		DestinationBucket: "bucket",
+		Destination:       "export.zip",
+	}))
+
+	require.Contains(t, cli.Buckets["bucket"], "export.zip")
+
+	data := cli.Buckets["bucket"]["export.zip"].Body
+
+	zr, err := zip.NewReader(bytes.NewReader(data), int64(len(data)))
+	require.NoError(t, err)
+
+	for _, path := range paths {
+		file, err := zr.Open(path.path)
+		require.NoError(t, err)
+
+		defer file.Close()
+
+		stat, err := file.Stat()
+		require.NoError(t, err)
+		require.Equal(t, path.size, stat.Size())
+
+		buf, err := io.ReadAll(file)
+		require.NoError(t, err)
+		require.Equal(t, cli.Buckets["bucket"][path.path].Body, buf)
+	}
+}
