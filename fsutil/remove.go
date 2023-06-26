cf842f4a8aab8380f9c3c7dd7a66dd98293c630f
Matt Hall <matt.hall@couchbase.com>
2023-06-26 09:25:22 +0100

    MB-57604 Remove misleading doc comment about logging
    A couple of functions said we logged where we do not.
    
    Change-Id: I534996ad6136c820cd4dae4845d984f982735bf9
    Reviewed-on: https://review.couchbase.org/c/tools-common/+/193035
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: James Lee <james.lee@couchbase.com>

--- fsutil/remove.go
+++ fsutil/remove.go
@@ -5,8 +5,6 @@ import (
 )
 
 // Remove the file/directory at the given path.
-//
-// NOTE: Logs in the event of an error since it's common to ignore the returned error.
 func Remove(path string, ignoreNotExists bool) error {
 	err := os.Remove(path)
 	if err == nil || ignoreNotExists && os.IsNotExist(err) {
@@ -17,8 +15,6 @@ func Remove(path string, ignoreNotExists bool) error {
 }
 
 // RemoveAll removes all the files/directories at the provided path.
-//
-// NOTE: Logs in the event of an error since it's common to ignore the returned error.
 func RemoveAll(path string) error {
 	err := os.RemoveAll(path)
 	if err == nil {
